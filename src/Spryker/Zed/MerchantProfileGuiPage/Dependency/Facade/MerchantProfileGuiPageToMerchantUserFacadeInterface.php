1623bfd05acc497f8425afab7e255320d293f760
ilyakubanov <ilya.kubanov@spryker.com>
2020-02-27 14:19:38 +0200

    BO-371: Added multiple merchant users support

--- src/Spryker/Zed/MerchantProfileGuiPage/Dependency/Facade/MerchantProfileGuiPageToMerchantUserFacadeInterface.php
+++ src/Spryker/Zed/MerchantProfileGuiPage/Dependency/Facade/MerchantProfileGuiPageToMerchantUserFacadeInterface.php
@@ -7,23 +7,12 @@
 
 namespace Spryker\Zed\MerchantProfileGuiPage\Dependency\Facade;
 
-use Generated\Shared\Transfer\MerchantUserCriteriaFilterTransfer;
 use Generated\Shared\Transfer\MerchantUserTransfer;
-use Generated\Shared\Transfer\UserTransfer;
 
 interface MerchantProfileGuiPageToMerchantUserFacadeInterface
 {
     /**
-     * @param \Generated\Shared\Transfer\MerchantUserCriteriaFilterTransfer $merchantUserCriteriaFilterTransfer
-     *
-     * @return \Generated\Shared\Transfer\MerchantUserTransfer|null
+     * @return \Generated\Shared\Transfer\MerchantUserTransfer
      */
-    public function findOne(
-        MerchantUserCriteriaFilterTransfer $merchantUserCriteriaFilterTransfer
-    ): ?MerchantUserTransfer;
-
-    /**
-     * @return \Generated\Shared\Transfer\UserTransfer
-     */
-    public function getCurrentUser(): UserTransfer;
+    public function getCurrentMerchantUser(): MerchantUserTransfer;
 }
